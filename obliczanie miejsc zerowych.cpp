1697e852a6b1cbdf7cf1924ad3908e4bfde4de73
Papcer <kacper.plata321@gmail.com>
2022-10-24 12:10:40 +0200

    obliczanie w postaci ax+by+c v1.4

--- obliczanie miejsc zerowych.cpp
+++ obliczanie miejsc zerowych.cpp
@@ -3,23 +3,48 @@ using namespace std;
 
 int main()
 {
-	double a,b,x;
-	cout << "Obliczanie miejsc zerowych funkcji ax + b " << endl;
-	cout << "Podaj a ";
-	cin >> a;
-	cout << "Podaj b ";
-	cin >> b;
-	cout << endl;
-	cout << "Funckja ma postac " << a <<"x + " << b << " = 0" << endl;  
-	if (a==0)
-	{
-		cout << "Brak miejsc zerowych funkcji ";
-	}
-	else if(a!=0 || b!=0)
-	{
-	 x=b/a ;	
-		cout << "Miejsce zerowe danej funkcji to " << x ;
-	}
-	
+	double a,b,x,c,q,w;
+	cout << "2. Obliczanie miejsc zerowych funkcji Ax + By + C = 0" << endl;
+		cout << "Podaj a ";
+		cin >> a;
+		cout << "Podaj b ";
+		cin >> b;
+		cout << "Podaj c ";
+		cin >> c;
+		cout << endl;
+		cout << "Funckja ma postac " << a <<"x + " << b << "y +" << c << " = 0"<< endl;  
+			
+		if (a == 0)
+		{
+			if (b==0)	
+			{
+				cout << "brak rozwiazan";
+			}
+			else
+			{
+				x = c/b; 
+				cout << "funckja stala y= " << x;
+			}
+		} 
+		else if (b==0)
+		{
+			if (c==0){
+				cout << "nieskoczenie wiele";
+			}	
+			else{
+				x = c/a;
+				cout << "miejsece zerowe wynosi x= " << x;
+			}
+		}
+		else if (c==0)
+		 {
+		 	cout << "wiele par";
+		 }
+		 	else{
+			q = ((-1)*a)/b;
+			w = ((-1)*c)/b;
+			x = w/q;
+			cout << x;
+		}
 	return 0;
 }

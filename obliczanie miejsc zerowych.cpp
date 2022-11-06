7ece31bbdc64e76fa5babce22ec691ad6df2ab28
papcer <kacper.plata321@gmail.com>
2022-11-06 17:44:30 +0100

    v2.0

--- obliczanie miejsc zerowych.cpp
+++ obliczanie miejsc zerowych.cpp
@@ -3,8 +3,17 @@ using namespace std;
 
 int main()
 {
+	int wybor;
 	double a,b,x,c,q,w;
-	cout << "2. Obliczanie miejsc zerowych funkcji Ax + By + C = 0" << endl;
+	double delta=0, x1=0, x2=0;
+	cout << "Wybierz rodzaj funckji" << endl;
+	cout << "1: Funkcja liniowa" << endl;
+	cout << "2: Funckja kwadratowa" << endl;
+	cin >> wybor;
+	
+	if (wybor == 1)
+	{
+		cout << "Obliczanie miejsc zerowych funkcji Ax + By + C = 0" << endl;
 		cout << "Podaj a ";
 		cin >> a;
 		cout << "Podaj b ";
@@ -12,39 +21,72 @@ int main()
 		cout << "Podaj c ";
 		cin >> c;
 		cout << endl;
-		cout << "Funckja ma postac " << a <<"x + " << b << "y +" << c << " = 0"<< endl;  
-			
+		cout << "Funckja ma postac " << a << "x + " << b << "y +" << c << " = 0" << endl;
+
 		if (a == 0)
 		{
-			if (b==0)	
+			if (b == 0)
 			{
 				cout << "brak rozwiazan";
 			}
 			else
 			{
-				x = c/b; 
+				x = c / b;
 				cout << "funckja stala y= " << x;
 			}
-		} 
-		else if (b==0)
+		}
+		else if (b == 0)
 		{
-			if (c==0){
+			if (c == 0) {
 				cout << "nieskoczenie wiele";
-			}	
-			else{
-				x = c/a;
+			}
+			else {
+				x = c / a;
 				cout << "miejsece zerowe wynosi x= " << x;
 			}
 		}
-		else if (c==0)
-		 {
-		 	cout << "wiele par";
-		 }
-		 	else{
-			q = ((-1)*a)/b;
-			w = ((-1)*c)/b;
-			x = w/q;
+		else if (c == 0)
+		{
+			cout << "wiele par";
+		}
+		else {
+			q = ((-1) * a) / b;
+			w = ((-1) * c) / b;
+			x = w / q;
 			cout << x;
 		}
+	}
+	else if (wybor == 2)
+	{
+		cout << "Obliczanie miejsc zerowych funkcji Ax^2 + Bx + C  = 0" << endl;
+		cout << "wartoœæ parametru a musi byæ ró¿na od 0" << endl ;
+		cout << "Podaj a ";
+		cin >> a;
+		cout << "Podaj b ";
+		cin >> b;
+		cout << "Podaj c ";
+		cin >> c;
+		cout << endl;
+		cout << "Funckja ma postac " << a << "x^2 + " << b << "x +" << c << " = 0" << endl;
+
+		delta = (b * b) - 4 * a * c;
+		cout << "Delta = " << delta << endl;
+		if (delta > 0)
+		{
+			x1 = (-1 * b - sqrt(delta)) / (2 * a);
+			x2 = (-1 * b + sqrt(delta)) / (2 * a);
+			cout << "Pierwsze miejsce zerowe: " << x1 << endl;
+			cout << "Drugie miejsce zerowe: " << x2 << endl;
+		}
+		else if (delta == 0) {
+			x1 = (- 1 * b) / (2 * a);
+		}
+		else {
+			cout << "Brak miejsce zerowych: ";
+		}
+	}
+
+
+		
 	return 0;
 }
